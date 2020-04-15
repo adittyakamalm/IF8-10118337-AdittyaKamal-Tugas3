51fdd297ed3dda392a8a7cf03033e549de1307e6
Adittya Kamal <adittyakamalm@gmail.com>
2020-04-15 12:33:23 +0700

    Tugas 3 Kalkulator

---
+++ src/kalkulator/Kalkulator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulator;
+
+/**
+ *
+ * @author aditt
+ */
+public class Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String bil;
+        bil = "";
+        framkal utama = new framkal();
+        utama.setTitle("Kalkulator Sederhana");
+        utama.setVisible(true);
+    }
+    
+}
---
+++ src/kalkulator/framekalkulator.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulator;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author aditt
+ */
+public class framekalkulator extends JFrame {
+    public framekalkulator()
+        {
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+    
+    public void tampil()
+    {
+        this.setVisible(true);
+    }
+    
+    public void tampil(int hasil)
+    {
+        this.setVisible(true);
+    }
+    
+}
